e772abee89f3bd38f05da5e2c19cc0ea4e19f280
ShriyanshiSharma <65887221+ShriyanshiSharma@users.noreply.github.com>
2021-07-09 11:50:01 -0700

    Create pass.py

---
+++ Random Password/pass.py
@@ -0,0 +1,22 @@
+import random
+ 
+wordlist =[]
+sc = ['@','#','#','$','*','&','!']
+
+with open("para.txt","r") as f:
+    data = f.readlines()
+
+    for line in data:
+        words = line.split()
+        
+        for item in words:
+            if len(item) > 5:
+                wordlist.append(item.capitalize())
+                
+word = random.choice(wordlist)
+sch = random.choice(sc)
+nm = str(random.randint(10,99))
+
+pas = word + sch + nm
+
+print(pas)
